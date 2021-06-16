a968b011c4e5509f28581e6a77f169cc1b0aa6ed
rvfintech <61241140+rvfintech@users.noreply.github.com>
2021-06-16 08:11:06 +0100

    SAL-147 #comment success and error components

--- components/RegisterYourInterests/ErrorInfo.js
+++ components/RegisterYourInterests/ErrorInfo.js
@@ -7,7 +7,7 @@ const ErrorInfo = () => {
         <div className="testimonials-area" style={{background: '#ff00005c'}}>
             <div className="section-title" style={{ color: '#ff2222' }}>               
                 <h6 style={{ color: '#ff2222' }}>There was an error trying to send your message.</h6>
-                <h6 style={{ color: '#ff2222' }}>Please try again later.</h6>
+                <h6 style={{ color: '#ff2222' }}>Please refresh the page and try again.</h6>
             </div>
         </div>
 
