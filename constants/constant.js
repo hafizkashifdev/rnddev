cd8854d53dc62b3b640967a5bc866a3b2d537ae6
Muhammad Muntazir Mehdi <mmmbukhari@gmail.com>
2021-06-23 15:08:10 +0500

    SAL-227

--- constants/constant.js
+++ constants/constant.js
@@ -1,13 +1,13 @@
-export const mondaymicrourl = 'https://monday.musikheist.com'
-export const airtablemicrourl = 'https://airtable.musikheist.com'
-export const twilliotablemicrourl = 'https://emailmicro.musikheist.com'
+export const mondaymicrourl = 'https://90k2z6uixf.execute-api.us-west-2.amazonaws.com'
+export const airtablemicrourl = 'https://rkoh18sgre.execute-api.us-west-2.amazonaws.com'
+export const twilliotablemicrourl = 'https://1ga6wy07u1.execute-api.us-west-2.amazonaws.com'
 export const emailhtml = (title, first_name, last_name, team) => {
     return `Hello ${title}  ${first_name} ${last_name} <br>
     Thanks for contacting us , As our customer relationship officer will get back to you shortly.<br>
     Regards <br>
     Team ${team}`
 }
-export const emailtitle= (appname)=>{
+export const emailtitle = (appname) => {
     return `Thanks for contacting us from ${appname}`
 }
 export const ContactBoardId = 1377220002
