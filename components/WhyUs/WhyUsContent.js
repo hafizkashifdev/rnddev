143d200cc7314d919cccb011561d97c9ef0291c5
Muhammad Zubair <zubair@orcalo.co.uk>
2021-08-20 18:21:01 +0500

    SAL-528 #comment why us styling

--- components/WhyUs/WhyUsContent.js
+++ components/WhyUs/WhyUsContent.js
@@ -3,88 +3,120 @@ import ServiceDetailsContent from './ServiceDetailsContent';
 
 const WhyUsContent = () => {
 return (
-<section className="privacy-policy-area">
+<section className="privacy-policy-area ptb-20">
     <div className="container">
         <div className="row">
-            <div className="col-lg-6 col-md-12">
+            <div class="col-sm-12">
+                <h3>The simplest way is the right way. R&D Tax credit claims simplified</h3>
+            </div>
+            <div className="col-lg-6">
                 <div className="privacy-policy-content">
-                    <h3>The simplest way is the right way. R&D Tax credit claims simplified</h3>
-                    <p>
-                    <ul>
-                        <li>
-                            <b>Reports completed fast and efficiently</b>
-                            <ul class="mt-0">
-                                o A professionally customised report for your R&D tax credit claim
-                            </ul>
-                        </li>
-                    </ul>
-                    <ul>
-                        <li>
-                            <b>HMRC Tax compliant</b>
-                            <ul class="mt-0">
-                                o Our team reassures you as we ensure HMRC Tax compliance
-                            </ul>
-                        </li>
-                    </ul>
-                    <ul>
-                        <li>
-                            <b>Fees lowest in the sector</b>
-                            <ul class="mt-0">
-                                o Fees from £300 with a ceiling of £1200 for claims under £100,000. Anything claim above
-                                £100,000 will be £1200 + 2.5% of amount above £100,000 with a ceiling of £7500.
-                            </ul>
-                        </li>
-                    </ul>
-                    <ul>
-                        <li>
-                            <b>Expert tax Adviser portal</b>
-                            <ul class="mt-0">
-                                o We provide an R&D Tax credit expert to take you through your claim.
-                            </ul>
-                        </li>
-                    </ul>
-                    <ul>
-                        <li>
-                            <b>Grants, SME and RDEC claims are confirmed automatically</b>
-                            <ul class="mt-0">
-                                o Our software is designed to simplify and maximise your R&D claim
-                            </ul>
-                        </li>
-                    </ul>
-                    <ul>
-                        <li>
-                            <b>Your time is our concern</b>
-                            <ul class="mt-0">
-                                o We offer a complete online service from start to finish to minimise your time on the
-                                claim and
-                                allow you to maintain your focus on running the business.
-                            </ul>
-                        </li>
-                    </ul>
-                    <ul>
-                        <li>
-                            <b>Calculations are generated in real-time.</b>
-                            <ul class="mt-0">
-                                o We give you an estimated figure at the start of your claim in order for you make a
-                                decision on
-                                proceeding with a claim and a detailed calculation at the end of the claim process.
-                            </ul>
-                        </li>
-                    </ul>
-                    <ul>
-                        <li>
-                           <b>Assist Accountants and Businesses</b>
-                            <ul class="mt-0">
-                                o We can provide the most innovative and cost-effective online tool to assist
-                                Accountants and
-                                Businesses through the web of the R&D Tax Credits process
-                            </ul>
-                        </li>
-                    </ul>
-                    </p>
+                    
+                    <div className="about-area">
+                        <div className="about-content">
+                            <div className="content">
+                                <ul className="about-list mb-0">
+                                    <li>
+                                        <i className="flaticon-tick"></i>
+                                        Reports completed fast and efficiently
+                                    </li>
+                                    <p>
+                                    <ul>
+                                        o A professionally customised report for your R&D tax credit claim
+                                    </ul>
+                                    </p>
+                                    <li>
+                                        <i className="flaticon-tick"></i>
+                                        HMRC Tax compliant
+                                    </li>
+                                    <p>
+                                    <ul>
+                                        o Our team reassures you as we ensure HMRC Tax compliance
+                                    </ul>
+                                    </p>
+                                    <li>
+                                        <i className="flaticon-tick"></i>
+                                        Fees lowest in the sector
+                                    </li>
+                                    <p>
+                                    <ul>
+                                        o Fees from £300 with a ceiling of £1200 for claims under £100,000. Anything
+                                        claim above
+                                        £100,000 will be £1200 + 2.5% of amount above £100,000 with a ceiling of £7500.
+                                    </ul>
+                                    </p>
+                                    <li>
+                                        <i className="flaticon-tick"></i>
+                                        Expert tax Adviser portal
+                                    </li>
+                                    <p>
+                                    <ul>
+                                        o We provide an R&D Tax credit expert to take you through your claim.
+                                    </ul>
+                                    </p>
+                                    
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="col-lg-6 col-md-12">
+            <div className="col-lg-6">
+                <div className="privacy-policy-content">
+                    <div className="about-area">
+                        <div className="about-content">
+                            <div className="content">
+                                <ul className="about-list mb-0">
+                                    <li>
+                                        <i className="flaticon-tick"></i>
+                                        Grants, SME and RDEC claims are confirmed automatically
+                                    </li>
+                                    <p>
+                                    <ul>
+                                        o Our software is designed to simplify and maximise your R&D claim
+                                    </ul>
+                                    </p>
+                                    <li>
+                                        <i className="flaticon-tick"></i>
+                                        Your time is our concern
+                                    </li>
+                                    <p>
+                                    <ul>
+                                        o We offer a complete online service from start to finish to minimise your time
+                                        on the claim and
+                                        allow you to maintain your focus on running the business.
+                                    </ul>
+                                    </p>
+                                    <li>
+                                        <i className="flaticon-tick"></i>
+                                        Calculations are generated in real-time.
+                                    </li>
+                                    <p>
+                                    <ul>
+                                        o We give you an estimated figure at the start of your claim in order for you
+                                        make a decision on
+                                        proceeding with a claim and a detailed calculation at the end of the claim
+                                        process.
+                                    </ul>
+                                    </p>
+                                    <li>
+                                        <i className="flaticon-tick"></i>
+                                        Assist Accountants and Businesses
+                                    </li>
+                                    <p>
+                                    <ul>
+                                        o We can provide the most innovative and cost-effective online tool to assist
+                                        Accountants and
+                                        Businesses through the web of the R&D Tax Credits process
+                                    </ul>
+                                    </p>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="col-lg-12">
                 <div className="privacy-policy-content pt-3">
                     <img src="/img/why-us-rnd.jpg" alt="" />
                 </div>
