2e825e436c601270a6c1a2c0a0ec910348f3dcd1
Muhammad Zubair <zubair@orcalo.co.uk>
2021-08-20 17:59:49 +0500

    SAL-526 #comment how it works styling

--- components/HowItWorks/HowItWorksContent.js
+++ components/HowItWorks/HowItWorksContent.js
@@ -12,20 +12,60 @@ return (
                         We are designed to provide our Clients with data accuracy, so they can make informed decisions. 
                         By following simple steps you can make your Tax process so much easier and efficient.
                     </h3>
-                    
-                    <p>
-                        <ul>
-                            <li>Registration</li>
-                            <li>Validation</li>
-                            <li>Eligibility Questionnaire</li>
-                            <li>Summary Report</li>
-                            <li>Detailed questionnaire</li>
-                            <li>Claim computation</li>
-                            <li>Seek Expert Advice</li>
-                            <li>Generate Detailed Report</li>
-                            <li>Submits the claims to HMRC</li>
-                        </ul>
-                    </p>
+                    <div className="about-area">
+                        <div className="about-content">
+                            <div className="row">
+                                <div class="col-sm-6">
+                                    <div className="content">
+                                        <ul className="about-list mb-0">
+                                            <li>
+                                                <i className="flaticon-tick"></i>
+                                                Registration
+                                            </li>
+                                            <li>
+                                                <i className="flaticon-tick"></i>
+                                                Validation
+                                            </li>
+                                            <li>
+                                                <i className="flaticon-tick"></i>
+                                                Eligibility Questionnaire
+                                            </li>
+                                            <li>
+                                                <i className="flaticon-tick"></i>
+                                                Summary Report
+                                            </li>
+                                            <li>
+                                                <i className="flaticon-tick"></i>
+                                                Detailed questionnaire
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div className="content">
+                                        <ul className="about-list mb-0">
+                                            <li>
+                                                <i className="flaticon-tick"></i>
+                                                Claim computation
+                                            </li>
+                                            <li>
+                                                <i className="flaticon-tick"></i>
+                                                Seek Expert Advice
+                                            </li>
+                                            <li>
+                                                <i className="flaticon-tick"></i>
+                                                Generate Detailed Report
+                                            </li>
+                                            <li>
+                                                <i className="flaticon-tick"></i>
+                                                Submits the claims to HMRC
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <div class="width-60-center">
                         <img src="/img/how-its-work.png" alt="" />
                     </div>
