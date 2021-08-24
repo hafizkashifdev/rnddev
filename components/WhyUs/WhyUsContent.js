51a31cbadc87cd9cb764f5a478c4494d7a27101d
Muhammad Zubair <zubair@orcalo.co.uk>
2021-08-24 17:31:04 +0500

    SAL-557 #comment update list style in why us page

--- components/WhyUs/WhyUsContent.js
+++ components/WhyUs/WhyUsContent.js
@@ -24,41 +24,31 @@ return (
                                         <i className="flaticon-tick"></i>
                                         Reports completed fast and efficiently
                                     </li>
-                                    <p>
-                                    <ul>
-                                        o A professionally customised report for your R&D tax credit claim
-                                    </ul>
+                                    <p className="li-in-li">
+                                        <i class="bx bx-badge-check"></i> A professionally customised report for your R&D tax credit claim
                                     </p>
                                     <li>
                                         <i className="flaticon-tick"></i>
                                         HMRC Tax compliant
                                     </li>
-                                    <p>
-                                    <ul>
-                                        o Our team reassures you as we ensure HMRC Tax compliance
-                                    </ul>
+                                    <p className="li-in-li">
+                                        <i class="bx bx-badge-check"></i> Our team reassures you as we ensure HMRC Tax compliance
                                     </p>
                                     <li>
                                         <i className="flaticon-tick"></i>
                                         Fees lowest in the sector
                                     </li>
-                                    <p>
-                                    <ul>
-                                        o Fees from £300 with a ceiling of £1200 for claims under £100,000. Anything
-                                        claim above
-                                        £100,000 will be £1200 + 2.5% of amount above £100,000 with a ceiling of £7500.
-                                    </ul>
+                                    <p className="li-in-li">
+                                        <i class="bx bx-badge-check"></i> Fees from £300 with a ceiling of £1200 for claims under £100,000. Anything
+                                        claim above £100,000 will be £1200 + 2.5% of amount above £100,000 with a ceiling of £7500.
                                     </p>
                                     <li>
                                         <i className="flaticon-tick"></i>
                                         Expert tax Adviser portal
                                     </li>
-                                    <p>
-                                    <ul>
-                                        o We provide an R&D Tax credit expert to take you through your claim.
-                                    </ul>
+                                    <p className="li-in-li">
+                                        <i class="bx bx-badge-check"></i> We provide an R&D Tax credit expert to take you through your claim.
                                     </p>
-                                    
                                 </ul>
                             </div>
                         </div>
@@ -75,44 +65,32 @@ return (
                                         <i className="flaticon-tick"></i>
                                         Grants, SME and RDEC claims are confirmed automatically
                                     </li>
-                                    <p>
-                                    <ul>
-                                        o Our software is designed to simplify and maximise your R&D claim
-                                    </ul>
+                                    <p className="li-in-li">
+                                        <i class="bx bx-badge-check"></i> Our software is designed to simplify and maximise your R&D claim
                                     </p>
                                     <li>
                                         <i className="flaticon-tick"></i>
                                         Your time is our concern
                                     </li>
-                                    <p>
-                                    <ul>
-                                        o We offer a complete online service from start to finish to minimise your time
-                                        on the claim and
-                                        allow you to maintain your focus on running the business.
-                                    </ul>
+                                    <p className="li-in-li">
+                                        <i class="bx bx-badge-check"></i> We offer a complete online service from start to finish to minimise your time
+                                        on the claim and allow you to maintain your focus on running the business.
                                     </p>
                                     <li>
                                         <i className="flaticon-tick"></i>
                                         Calculations are generated in real-time.
                                     </li>
-                                    <p>
-                                    <ul>
-                                        o We give you an estimated figure at the start of your claim in order for you
-                                        make a decision on
-                                        proceeding with a claim and a detailed calculation at the end of the claim
-                                        process.
-                                    </ul>
+                                    <p className="li-in-li">
+                                        <i class="bx bx-badge-check"></i> We give you an estimated figure at the start of your claim in order for you
+                                        make a decision on proceeding with a claim and a detailed calculation at the end of the claim process.
                                     </p>
                                     <li>
                                         <i className="flaticon-tick"></i>
                                         Assist Accountants and Businesses
                                     </li>
-                                    <p>
-                                    <ul>
-                                        o We can provide the most innovative and cost-effective online tool to assist
-                                        Accountants and
-                                        Businesses through the web of the R&D Tax Credits process
-                                    </ul>
+                                    <p className="li-in-li">
+                                        <i class="bx bx-badge-check"></i> We can provide the most innovative and cost-effective online tool to assist
+                                        Accountants and Businesses through the web of the R&D Tax Credits process
                                     </p>
                                 </ul>
                             </div>
